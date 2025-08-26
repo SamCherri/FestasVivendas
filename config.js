bed3401b6afa1302203c0a19acbafa4c0c9b9d85
SamCherri <36168102+SamCherri@users.noreply.github.com>
2025-08-26 19:56:37 -0300

    Atualizar o config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
-// config.js v8 - Firebase (Auth + Firestore)
+// config.js v8.2 - Firebase (Auth + Firestore)
 export const CONFIG = {
-  halls: ['Gourmet', 'Menor'],   // adicione mais se quiser
+  halls: ['Gourmet', 'Menor'],
   firebaseConfig: {
     apiKey: "AIzaSyB3PHKo18JjC6M-MJLsE431yxHnAZhWdzo",
     authDomain: "festasvivendas.firebaseapp.com",
     projectId: "festasvivendas",
-    storageBucket: "festasvivendas.firebasestorage.app",
+    storageBucket: "festasvivendas.appspot.com", // corrigido
     messagingSenderId: "460753271698",
     appId: "1:460753271698:web:984421ed1040d00c2a1f4b",
     measurementId: "G-4ZN38WR16K"
