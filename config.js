ea7e8234316e4fbc3cd724f813c02ed39a456979
SamCherri <36168102+SamCherri@users.noreply.github.com>
2025-08-27 20:30:09 -0300

    Atualizar o config.js

--- config.js
+++ config.js
@@ -4,16 +4,12 @@ export const CONFIG = {
     apiKey: "AIzaSyB3PHKo18JjC6M-MJLsE431yxHnAZhWdzo",
     authDomain: "festasvivendas.firebaseapp.com",
     projectId: "festasvivendas",
-    storageBucket: "festasvivendas.appspot.com", // <- corrigido
+    storageBucket: "festasvivendas.appspot.com", // <- correto
     messagingSenderId: "460753271698",
     appId: "1:460753271698:web:984421ed1040d00c2a1f4b",
     measurementId: "G-4ZN38WR16K"
   },
-
-  // Salões disponíveis no formulário/filtros
   halls: ["Gourmet", "Menor"],
-
-  // Cores e nome da marca
   brand: {
     name: "Vivendas de La Salle",
     primary: "#0B74B8",
