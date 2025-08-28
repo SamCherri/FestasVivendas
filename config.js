01281b8887340a5c704bb87749e132dee532b0d9
SamCherri <36168102+SamCherri@users.noreply.github.com>
2025-08-28 10:47:38 -0300

    Atualizar o config.js

--- config.js
+++ config.js
@@ -1,8 +1,17 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyDsMEbeVz-niQVM3GydnycJ0HcvLWNikw4",
-  authDomain: "festavivendas.firebaseapp.com",
-  projectId: "festavivendas",
-  storageBucket: "festavivendas.appspot.com",
-  messagingSenderId: "722983983574",
-  appId: "1:722983983574:web:cb8e7da44fb43a87e4ac77"
+// config.js v26 — corrigido
+export const CONFIG = {
+  firebaseConfig: {
+    apiKey: "AIzaSyDsMEbeVz-niQVM3GydnycJ0HcvLWNikw4",
+    authDomain: "festavivendas.firebaseapp.com",
+    projectId: "festavivendas",
+    storageBucket: "festavivendas.appspot.com",   // ✅ corrigido aqui
+    messagingSenderId: "722983983574",
+    appId: "1:722983983574:web:cb8e7da44fb43a87e4ac77"
+  },
+  halls: ["Gourmet", "Menor"],
+  brand: {
+    name: "Vivendas de La Salle",
+    primary: "#0B74B8",
+    secondary: "#18c07a"
+  }
 };
