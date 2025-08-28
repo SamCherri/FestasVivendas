328f0c3b63ed852bb0aec07a85f9efdbf1aec8ec
SamCherri <36168102+SamCherri@users.noreply.github.com>
2025-08-28 10:43:05 -0300

    Atualizar o config.js

--- config.js
+++ config.js
@@ -1,18 +1,8 @@
-// config.js — Projeto: FestaVivendas (produção)
-export const CONFIG = {
-  firebaseConfig: {
-    apiKey: "AlzaSyDsMEbeVz-niQVM3GydnycJ0HcvLWNikw4",
-    authDomain: "festavivendas.firebaseapp.com",
-    projectId: "festavivendas",
-    storageBucket: "festavivendas.appspot.com",
-    messagingSenderId: "722983983574",
-    appId: "1:722983983574:web:cb8e7da44fb43a87e4ac77"
-    // measurementId opcional
-  },
-  halls: ["Gourmet", "Menor"],
-  brand: {
-    name: "Vivendas de La Salle",
-    primary: "#0B74B8",
-    secondary: "#18C07A"
-  }
+const firebaseConfig = {
+  apiKey: "AIzaSyDsMEbeVz-niQVM3GydnycJ0HcvLWNikw4",
+  authDomain: "festavivendas.firebaseapp.com",
+  projectId: "festavivendas",
+  storageBucket: "festavivendas.appspot.com",
+  messagingSenderId: "722983983574",
+  appId: "1:722983983574:web:cb8e7da44fb43a87e4ac77"
 };
