797423e1212b244bb753045f9fd4a0d6f4a87661
SamCherri <36168102+SamCherri@users.noreply.github.com>
2025-08-28 09:25:38 -0300

    Atualizar o config.js

--- config.js
+++ config.js
@@ -1,17 +1,13 @@
 // config.js
-export const CONFIG = {
-  firebaseConfig: {
-    apiKey: "AIzaSyDSMebVZ-n10W3GydnycJ0HcWLWlnkw4",
-    authDomain: "festavivendas.firebaseapp.com",
-    projectId: "festavivendas",
-    storageBucket: "festavivendas.appspot.com",
-    messagingSenderId: "72298393574",
-    appId: "1:72298393574:web:cb8e7da44fb43a87e4ac77"
-  },
-  halls: ["Gourmet", "Menor"],
-  brand: {
-    name: "Vivendas de La Salle",
-    primary: "#0B74B8",
-    secondary: "#18c07a"
-  }
+
+const firebaseConfig = {
+  apiKey: "AlzaSyDsMEbeVz-niQVM3GydnycJ0HcvLWNikw4",
+  authDomain: "festavivendas.firebaseapp.com",
+  projectId: "festavivendas",
+  storageBucket: "festavivendas.appspot.com",
+  messagingSenderId: "722983983574",
+  appId: "1:722983983574:web:cb8e7da44fb43a87e4ac77"
 };
+
+// Inicializa o Firebase
+firebase.initializeApp(firebaseConfig);
