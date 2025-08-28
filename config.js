46fd35037a35459aba76ba237989fc31716c939a
SamCherri <36168102+SamCherri@users.noreply.github.com>
2025-08-28 11:58:51 -0300

    Atualizar o config.js tava funcionando antes

--- config.js
+++ config.js
@@ -1,10 +1,9 @@
-// config.js v26 — corrigido
 export const CONFIG = {
   firebaseConfig: {
     apiKey: "AIzaSyDsMEbeVz-niQVM3GydnycJ0HcvLWNikw4",
     authDomain: "festavivendas.firebaseapp.com",
     projectId: "festavivendas",
-    storageBucket: "festavivendas.appspot.com",   // ✅ corrigido aqui
+    storageBucket: "festavivendas.appspot.com",
     messagingSenderId: "722983983574",
     appId: "1:722983983574:web:cb8e7da44fb43a87e4ac77"
   },
