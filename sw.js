78e39fc2ca06c0dfae857cfd39b6c5a374905e47
SamCherri <36168102+SamCherri@users.noreply.github.com>
2025-08-29 20:02:16 -0300

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -1,32 +1,15 @@
-const CACHE = "festas-v3";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./app.js",
-  "./config.js",
-  "./manifest.webmanifest",
-  "./favicon.svg",
-  "./icon-192.png",
-  "./icon-512.png"
-];
+const CACHE = "festas-v1";
+const ASSETS = ["./","./index.html","./style.css","./app.js","./config.js","./manifest.webmanifest","./favicon.svg"];
 
-self.addEventListener("install",(e)=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-self.addEventListener("activate",(e)=>{
-  e.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
-  );
-  self.clients.claim();
-});
+self.addEventListener("install",(e)=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener("activate",(e)=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener("fetch",(e)=>{
+  const req=e.request;
   e.respondWith(
-    fetch(e.request).then(res=>{
+    fetch(req).then(res=>{
       const copy=res.clone();
-      caches.open(CACHE).then(c=>c.put(e.request,copy));
+      caches.open(CACHE).then(c=>c.put(req,copy));
       return res;
-    }).catch(()=>caches.match(e.request).then(m=>m||caches.match("./index.html")))
+    }).catch(()=>caches.match(req).then(m=>m||caches.match("./index.html")))
   );
 });
