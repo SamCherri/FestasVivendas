856904742fb9d025806eb115d727c7e9ee7c5534
SamCherri <36168102+SamCherri@users.noreply.github.com>
2025-08-30 09:10:59 -0300

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "festas-v3";
+const CACHE = "festas-v1";
 const ASSETS = ["./","./index.html","./style.css","./app.js","./config.js","./manifest.webmanifest","./favicon.svg"];
 
 self.addEventListener("install",(e)=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
